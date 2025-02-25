dda9f71c40fb6663a7cf11413edc8dd69a4bfe19
siendesign <victoryessien01@gmail.com>
2025-02-25 20:50:25 +0100

    updt: now im frustrated--2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,41 @@
-import Image from "next/image";
-
 "use client";
 
 import dynamic from "next/dynamic";
+import { Metadata } from "next";
 
 const Demo = dynamic(() => import("@/components/Demo"), {
   ssr: false,
 });
+
+const appUrl = "https://farcaster-frames-test-tan.vercel.app";
+
+const frame = {
+  version: "next",
+  imageUrl: `${appUrl}/image.png`,
+  button: {
+    title: "Lunch Frame",
+    action: {
+      type: "lunch_frame",
+      name: "farcaster test app",
+      url: appUrl,
+      splashImageUrl: `${appUrl}/image.png`,
+      splashBackgroundColor: "#000",
+    },
+  },
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title:"test farcaster frame",
+    openGraph:{
+      title:"test farcaster frame",
+      description:"test of farcaster frames"
+    },
+    other:{
+      "fc:frame": JSON.stringify(frame)
+    }
+  };
+}
 
 export default function Home() {
   return (
