f3f547cd12ddc6fe6cf379b3de6d7de39d34ab3f
siendesign <victoryessien01@gmail.com>
2025-02-26 11:50:52 +0100

    updt:arrrrgggghhhh.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-
+'use client';
 import dynamic from "next/dynamic";
 import { Metadata } from "next";
 
@@ -8,32 +8,32 @@ const Demo = dynamic(() => import("@/components/Demo"), {
 
 const appUrl = "https://farcaster-frames-test-tan.vercel.app";
 
-const frame = {
-  version: "next",
-  imageUrl: `${appUrl}/image.png`,
-  button: {
-    title: "Lunch Frame",
-    action: {
-      type: "lunch_frame",
-      name: "farcaster test app",
-      url: appUrl,
-      splashImageUrl: `${appUrl}/image.png`,
-      splashBackgroundColor: "#000",
-    },
-  },
-};
+// const frame = {
+//   version: "next",
+//   imageUrl: `${appUrl}/image.png`,
+//   button: {
+//     title: "Lunch Frame",
+//     action: {
+//       type: "lunch_frame",
+//       name: "farcaster test app",
+//       url: appUrl,
+//       splashImageUrl: `${appUrl}/image.png`,
+//       splashBackgroundColor: "#000",
+//     },
+//   },
+// };
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title:"test farcaster frame",
-    openGraph:{
-      title:"test farcaster frame",
-      description:"test of farcaster frames"
-    },
-    other:{
-      "fc:frame": JSON.stringify(frame)
-    }
-  };
+// export async function generateMetadata(): Promise<Metadata> {
+//   return {
+//     title:"test farcaster frame",
+//     openGraph:{
+//       title:"test farcaster frame",
+//       description:"test of farcaster frames"
+//     },
+//     other:{
+//       "fc:frame": JSON.stringify(frame)
+//     }
+//   };
 }
 
 export default function Home() {
